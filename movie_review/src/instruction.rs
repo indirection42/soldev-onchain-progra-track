44edec8af2ff6b11e8fde82068005dfdfe4d94d8
indirection42 <jiyuanz95@gmail.com>
2024-04-03 15:25:52 +0800

    module1.2(handle instruction data): lab finished

---
+++ movie_review/src/instruction.rs
@@ -0,0 +1,34 @@
+use borsh::BorshDeserialize;
+use solana_program::program_error::ProgramError;
+
+pub enum MovieInstruction {
+    AddMovieReview {
+        title: String,
+        rating: u8,
+        description: String,
+    },
+}
+
+#[derive(BorshDeserialize)]
+struct MovieReviewPayload {
+    title: String,
+    rating: u8,
+    description: String,
+}
+
+impl MovieInstruction {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (&variant, rest) = input
+            .split_first()
+            .ok_or(ProgramError::InvalidInstructionData)?;
+        let instruction = MovieReviewPayload::try_from_slice(rest)?;
+        Ok(match variant {
+            0 => MovieInstruction::AddMovieReview {
+                title: instruction.title,
+                rating: instruction.rating,
+                description: instruction.description,
+            },
+            _ => return Err(ProgramError::InvalidInstructionData),
+        })
+    }
+}
